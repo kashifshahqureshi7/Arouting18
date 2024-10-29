e9f4fd022ded4a2c8d0bff077765eada1007511d
Muhammad Hassan <mhassan4641@gmail.com>
2024-10-29 17:53:15 +0500

    initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { AboutComponent } from './about/about.component';
+import { ContactusComponent } from './contactus/contactus.component';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path:'',redirectTo:'home',pathMatch:'full'},
+  { path: 'home', component: DashboardComponent,title:'Home page' },
+  { path: 'about', component: AboutComponent,title:'About Us' },
+  { path: 'contactus', component: ContactusComponent, title: 'Contact Us' },
+  {path:'**',component:PagenotfoundComponent,title:'Page Not Found'}
+];
